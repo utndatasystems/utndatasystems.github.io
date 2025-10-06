cb0b18fdf9478246daa9dd65784e9840c99f8ad9
andizimmerer <andreas-zimmerer@outlook.de>
2025-10-06 12:07:28 +0000

    Deploying to gh-pages from @ utndatasystems/utndatasystems.github.io@ac15bb011087fcbd51684900b66356c47df61c33 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -2,5 +2,15 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   window.dataLayer.push(arguments);
 }
+
+// Deny all storage by default to prevent cookies
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'analytics_storage': 'denied'
+});
+
 gtag("js", new Date());
-gtag("config", "");
+gtag("config", "G-01T0SBSGBC", {
+    'client_storage': 'none',
+    'anonymize_ip': true
+});
